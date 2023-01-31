216e6ae1b1365d1c3845e3d42b6a63a8ed5bb485
KostasEreksonas <k.ereksonas@gmail.com>
2023-01-31 15:43:58 +0200

    Moved scripts from Portfolio repository

---
+++ tmp/init-script.js
@@ -0,0 +1,21 @@
+// Pirma uzduotis
+let a = 1;
+console.log('Kintamojo a reikšmė: ' + a);
+a += 1;
+console.log('Antnaujinta kintamojo a reikšmė: ' + a);
+
+// Antra uzduotis
+let b = "Kostas Ereksonas";
+console.log('Mano vardas: ' + b);
+
+// Trecia uzduotis
+let metai = 1998;
+let menuo = 08;
+let diena = 22;
+let gimimoData = 'Gimimo data: ' + diena + '.' + menuo + '.' + metai;
+let sum = metai % 10 + menuo % 10 + diena % 10;
+console.log('Gimimo data: ' + gimimoData + ' Paskutinių skaitmenų suma: ' + sum)
+
+images = document.getElementsByClassName('project-img')
+
+console.log(images)
